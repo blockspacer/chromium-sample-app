2df12ea952450bd4ce06b93326574b3453bf6522
Dreamer Dead <dreamer.dead@gmail.com>
2016-02-26 19:14:01 +0300

    SampleApp: add sample API code.

---
+++ src/sample_api.cc
@@ -0,0 +1,9 @@
+#include "sample_app/src/sample_api.h"
+
+namespace sample_api {
+
+bool CallApiFunction() {
+  return true;
+}
+
+}  // namespace sample_api
---
+++ src/sample_api.h
@@ -0,0 +1,11 @@
+#ifndef SAMPLE_APP_SAMPLE_API_H_
+#define SAMPLE_APP_SAMPLE_API_H_
+
+namespace sample_api {
+
+// Do some black magic.
+bool CallApiFunction();
+
+}  // namespace sample_api
+
+#endif  // SAMPLE_APP_SAMPLE_API_H_
