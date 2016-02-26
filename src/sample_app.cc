414a17651ef6b2c6c4d37b61a0106a91f2538da4
Dreamer Dead <dreamer.dead@gmail.com>
2016-02-26 18:17:26 +0300

    SampleApp: add command line sample.

--- src/sample_app.cc
+++ src/sample_app.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 
 namespace {
@@ -13,12 +16,34 @@ void StringsSample() {
             << std::endl;
 }
 
+void CommandLineSample() {
+  using base::CommandLine;
+
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+
+  std::cout << "Application program name is "
+            << command_line.GetProgram().AsUTF8Unsafe() << std::endl;
+
+  if (command_line.HasSwitch("bool-switch")) {
+    std::cout << "Detected a boolean switch!" << std::endl;
+  }
+
+  std::string string_switch = command_line.GetSwitchValueASCII("string-switch");
+  if (!string_switch.empty()) {
+    std::cout << "Got a string switch value: " << string_switch << std::endl;
+  }
+}
+
 }  // namespace
 
 int main(int argc, const char* argv[]) {
+  CHECK(base::CommandLine::Init(argc, argv))
+      << "Failed to parse a command line argument.";
+
   std::cout << "Hello from SampleApp!" << std::endl;
 
   StringsSample();
+  CommandLineSample();
 
   return 0;
 }
