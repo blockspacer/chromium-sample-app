bc6516173752fc9959fabbba54aedf99e330b417
Dreamer Dead <dreamer.dead@gmail.com>
2016-02-26 18:02:14 +0300

    SampleApp: add strings conversion sample.

--- src/sample_app.cc
+++ src/sample_app.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <string>
 
+#include "base/strings/utf_string_conversions.h"
+
+namespace {
+
+void StringsSample() {
+  std::cout << base::WideToUTF8(L"This is a wide string.") << std::endl;
+  std::wcout << base::UTF8ToWide("This is an UTF8 string.") << std::endl;
+  std::cout << base::UTF16ToUTF8(base::UTF8ToUTF16(
+                   "This is an UTF8 string converted to UTF16 and back."))
+            << std::endl;
+}
+
+}  // namespace
+
 int main(int argc, const char* argv[]) {
   std::cout << "Hello from SampleApp!" << std::endl;
+
+  StringsSample();
 
   return 0;
 }
