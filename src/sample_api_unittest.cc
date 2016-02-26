c324b4a0e9fdf867f3be10d195f9d52e2cd1b4a8
Dreamer Dead <dreamer.dead@gmail.com>
2016-02-26 19:23:58 +0300

    SampleApp: add sample_api unit tests.

---
+++ src/sample_api_unittest.cc
@@ -0,0 +1,15 @@
+#include "sample_app/src/sample_api.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace sample_api {
+
+namespace {
+
+TEST(SampleApi, ApiFunctionTest) {
+  EXPECT_TRUE(CallApiFunction());
+}
+
+}  // namespace
+
+}  // namespace sample_api
